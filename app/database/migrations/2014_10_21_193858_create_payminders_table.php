014e759c3795e1317d2eebf40290513747c9c84e
Duco Visbeen <duco@maximum.com>
2014-10-21 21:46:36 +0200

    migrations

---
+++ app/database/migrations/2014_10_21_193858_create_payminders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePaymindersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('payminders', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('sender_name');
+            $table->string('sender_iban');
+            $table->integer('start_time');
+            $table->integer('end_time');
+            $table->string('ip_address');
+            $table->string('geo');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('payminders');
+	}
+
+}
