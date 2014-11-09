c76ffcb6209c1bc5af69645a9331dccc5f9ed289
Duco Visbeen <duco@maximum.com>
2014-11-09 13:45:56 +0100

    live

--- app/controllers/PaymindersController.php
+++ app/controllers/PaymindersController.php
@@ -14,12 +14,12 @@ class PaymindersController extends \BaseController {
         $input = json_decode(base64_decode($payload));
 
         $payminder = new Payminder();
-        $payminder->sender_name = DB::getPdo()->quote($input->sender);
-        $payminder->sender_iban = DB::getPdo()->quote($input->iban);
+        $payminder->sender_name = $input->sender;
+        $payminder->sender_iban = $input->iban;
         $payminder->start_time = intval($input->startTime);
         $payminder->end_time = intval($input->sendTime);
         $payminder->ip_address = Request::getClientIp();
-        $payminder->description = DB::getPdo()->quote($input->description_p);
+        $payminder->description = $input->description_p;
         $payminder->save();
 
         $payminder->hash = Hash::make($payminder->id . microtime());
@@ -41,11 +41,11 @@ class PaymindersController extends \BaseController {
             foreach($input->personList as $friendinput)
             {
                 $friend = new Friend();
-                $friend->first_name = DB::getPdo()->quote($friendinput->firstname);
-                $friend->last_name = DB::getPdo()->quote($friendinput->lastname);
+                $friend->first_name = $friendinput->firstname;
+                $friend->last_name = $friendinput->lastname;
                 $friend->payminder_id = $payminder->id;
-                $friend->phonenumber = DB::getPdo()->quote($friendinput->phone);
-                $friend->amount = DB::getPdo()->quote($friendinput->amount);
+                $friend->phonenumber = $friendinput->phone;
+                $friend->amount = $friendinput->amount;
                 $friend->save();
 
                 $text = urlencode("Hallo, " . $friend->first_name . "!");
