b9d4cd0883498d4691e57a2ab5ea5e8ae19c167d
Duco Visbeen <duco@maximum.com>
2014-11-11 19:28:44 +0100

    urlencode fix

--- app/views/show.blade.php
+++ app/views/show.blade.php
@@ -7,7 +7,7 @@
         @import url(//fonts.googleapis.com/css?family=Lato:700);
 
         body {
-            margin:20px;
+            margin-top:20px;
             font-family:'Lato', sans-serif;
             text-align:left;
             color: #000;
@@ -38,12 +38,12 @@
     Datum: {{ date('d/m/y', $payminder->end_time); }}
 </p>
 <p>
-    <hr>
+    <hr style="margin-left:20px; margin-right:20px;">
 </p>
 <p>
     @foreach($friends as $friend)
-    <div style="width:100%; height:100px; background:#bcd42a; border-bottom-width: 1px; border-bottom-color: #000000; vertical-align: middle;">
-        <span style="text-align: left; margin-left: 20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="text-align:right">bedrag €{{ $friend->amount }}</span>
+    <div style="width:100%; height:60px; background:#bcd42a; border-bottom-width: 1px; border-bottom-color: #000000; vertical-align: middle; margin-top:1px;">
+        <span style="text-align: left; margin-left: 20px; margin-top:20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="text-align:right">bedrag €{{ $friend->amount }}</span>
     </div>
     @endforeach
 </p>
