dc12ba66a4cf70cf1788d11d3c51eab9ba016d66
Duco Visbeen <duco@maximum.com>
2014-11-12 12:22:22 +0100

    fixed database int to string

--- app/database/migrations/2014_10_21_193858_create_payminders_table.php
+++ app/database/migrations/2014_10_21_193858_create_payminders_table.php
@@ -17,7 +17,7 @@ class CreatePaymindersTable extends Migration {
 			$table->increments('id');
             $table->string('sender_name');
             $table->string('sender_iban');
-            $table->integer('start_time');
+            $table->string('start_time');
             $table->integer('end_time');
             $table->string('ip_address');
             $table->string('geo');
