57cd4caa0499225636fa0d9422ee484664480dc3
Duco Visbeen <duco@maximum.com>
2014-12-01 16:53:01 +0100

    colorfix

--- app/views/paid.blade.php
+++ app/views/paid.blade.php
@@ -1,4 +1,8 @@
 <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
-<br><br><br><center><h2>Super, je hebt betaald!</h2>
-<br>
-<br><a href="{{ action('friend.setnotpaid', null, [$id]) }}">Toch niet betaald? Klik hier</a></center>
+<body style="background-color: #fa5d4e">
+    <br><br><center>
+    <img src="http://api.payminder.nl/logo.jpg">
+    <br><h2 style="color: white">Super, je hebt betaald!</h2>
+    <br>
+    <br><a href="{{ action('friend.setnotpaid', null, [$id]) }}">Toch niet betaald? Klik hier</a></center>
+</body>
