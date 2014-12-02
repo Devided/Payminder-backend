f19268662a82a9f58537bea11fa74bab032b699a
Duco Visbeen <duco@maximum.com>
2014-12-02 17:36:57 +0100

    added text + app download link

--- app/views/paid.blade.php
+++ app/views/paid.blade.php
@@ -4,9 +4,9 @@
     <img src="http://api.payminder.nl/logo.jpg" style="width: 80%">
     <br><h2 style="color: white">Super, je hebt betaald!</h2>
     <br>
-    <br><a href="{{ action('friend.setnotpaid', [$id]) }}">Toch niet betaald? Klik hier</a>
+    <br>Nog niet betaald maar  per ongeluk op deze pagina beland? Laat dit aan je vriend(in) weten door op <a href="{{ action('friend.setnotpaid', [$id]) }}">deze link te klikken</a>
         <br><br><br><br><br>
         <hr>
-        <a href="https://www.facebook.com/pages/Payminder/712891918796289">Facebook</a>
+        <a href="https://www.facebook.com/pages/Payminder/712891918796289">Facebook</a> | <a href="">Download app</a>
     </center>
 </body>
