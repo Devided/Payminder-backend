ba518733110b33fcd51952121b0e848d03aa6559
Duco Visbeen <duco@maximum.com>
2014-12-04 16:04:09 +0100

    fixed width

--- app/views/paid.blade.php
+++ app/views/paid.blade.php
@@ -1,7 +1,7 @@
 <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
 <body style="background-color: #fa5d4e">
     <br><br><center>
-    <img src="http://api.payminder.nl/logo.jpg" style="width: 80%">
+    <img src="http://api.payminder.nl/logo.jpg" style="width: 100%">
     <br><h2 style="color: white">Super, je hebt betaald!</h2>
     <br>
     <br>Nog niet betaald maar  per ongeluk op deze pagina beland? Laat dit aan je vriend(in) weten door op <a href="{{ action('friend.setnotpaid', [$id]) }}">deze link te klikken</a>
