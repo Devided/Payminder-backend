a61127efe70f51b20dd8f902c77c1ac1e06a8189
Duco Visbeen <duco@maximum.com>
2014-12-04 16:07:38 +0100

    fixed br

--- app/views/paid.blade.php
+++ app/views/paid.blade.php
@@ -2,7 +2,7 @@
 <body style="background-color: #fa5d4e; margin: 0px; padding:0px;">
 <center>
     <img src="http://api.payminder.nl/logo.jpg" style="width: 100%">
-    <br><h2 style="color: white">Super, je hebt betaald!</h2>
+    <br><br><h2 style="color: white">Super, je hebt betaald!</h2>
     <br><span style="padding:20px; color:white;">Nog niet betaald maar  per ongeluk op deze pagina beland? Laat dit aan je vriend(in) weten door op <a href="{{ action('friend.setnotpaid', [$id]) }}">deze link te klikken</a></span>
         <br><br><br><br><br><br><br>
         <hr>
