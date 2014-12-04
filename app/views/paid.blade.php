daebec11a140c51f5e1446fefc95a6be202196ff
Duco Visbeen <duco@maximum.com>
2014-12-04 16:06:53 +0100

    fixed br

--- app/views/paid.blade.php
+++ app/views/paid.blade.php
@@ -1,11 +1,10 @@
 <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
 <body style="background-color: #fa5d4e; margin: 0px; padding:0px;">
-    <br><br><center>
+<center>
     <img src="http://api.payminder.nl/logo.jpg" style="width: 100%">
     <br><h2 style="color: white">Super, je hebt betaald!</h2>
-    <br>
     <br><span style="padding:20px; color:white;">Nog niet betaald maar  per ongeluk op deze pagina beland? Laat dit aan je vriend(in) weten door op <a href="{{ action('friend.setnotpaid', [$id]) }}">deze link te klikken</a></span>
-        <br><br><br><br><br>
+        <br><br><br><br><br><br><br>
         <hr>
         <a href="https://www.facebook.com/pages/Payminder/712891918796289">Facebook</a> | <a href="http://bit.ly/10ZNepH">Download app</a>
     </center>
