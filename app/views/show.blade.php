9d859ceab056744cc8519c7f1c321ef5a1689594
Duco Visbeen <duco@maximum.com>
2015-02-09 18:00:54 +0100

    t

--- app/views/show.blade.php
+++ app/views/show.blade.php
@@ -48,11 +48,11 @@
     @foreach($friends as $friend)
     @if(Friend::find($friend->id)->paid)
     <div style="width:100%; height:60px; background:#67ff6e; border-bottom-width: 1px; border-bottom-color: #000000; vertical-align: middle; margin-top:1px; padding-top:35px;">
-        <span style="text-align: left; margin-left: 20px; margin-top:20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="position:absolute; text-align:right; right:0px; margin-right:20px;">bedrag €{{ $friend->amount }}</span>
+        <span style="text-align: left; margin-left: 20px; margin-top:20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="position:absolute; text-align:right; right:0px; margin-right:20px;">€{{ $friend->amount }}</span>
     </div>
     @else
     <div style="width:100%; height:60px; background:#ff5c4f; border-bottom-width: 1px; border-bottom-color: #000000; vertical-align: middle; margin-top:1px; padding-top:35px;">
-        <span style="text-align: left; margin-left: 20px; margin-top:20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="position:absolute; text-align:right; right:0px; margin-right:20px;">bedrag €{{ $friend->amount }}</span>
+        <span style="text-align: left; margin-left: 20px; margin-top:20px;">{{ $friend->first_name }} {{ $friend->last_name }}</span><span style="position:absolute; text-align:right; right:0px; margin-right:20px;">€{{ $friend->amount }}</span>
     </div>
     @endif
     @endforeach
